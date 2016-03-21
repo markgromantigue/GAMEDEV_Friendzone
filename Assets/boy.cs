605a3467ab911011a15cf4c850a99b861f99bf54
markgromantigue <markg.romantigue@gmail.com>
2016-03-21 13:44:30 +0800

    Initial Commit

---
+++ Assets/boy.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boy : MonoBehaviour {
+
+	public int timer;
+	public float jumpForce;
+	public bool duck;
+	private Vector2 duc;
+	private Rigidbody2D rb2d;
+    private Animator animator;
+    private Vector2 charPos;
+
+	// Use this for initialization
+	void Start () {
+		duck = false;
+		timer = 0;
+		duc = transform.localScale;
+		rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        charPos = transform.position;
+		if (timer == 100) {
+			duck = false;
+			timer = 0;
+			duc.y = 1;
+			transform.localScale = duc;
+		} else if (timer != 0 && timer < 100) {
+			timer++;
+		}
+        
+        if (charPos.y <= -1.5f) {
+			animator.SetBool("GBJumping", false);
+		}else{
+            animator.SetBool("GBJumping", true);
+        }
+
+		if (Input.GetKeyDown("up")) {
+            animator.SetBool("GBJumping", true);
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+		}
+
+		if (Input.GetKeyDown("down") && !duck) {
+			duck = true;
+			duc.y = duc.y - 0.5f;
+			timer++;
+			transform.localScale = duc;
+		}
+	
+	}
+}
---
+++ Assets/scroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scroller : MonoBehaviour {
+
+	public float speed;
+	private Vector2 newPos;
+
+	// Use this for initialization
+	void Start () {
+		newPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newPos.x = newPos.x - speed;
+		transform.position = newPos;
+		if (newPos.x < -21.71f) {
+			newPos.x = 21.73f;
+			transform.position = newPos;
+		}
+	}
+}
