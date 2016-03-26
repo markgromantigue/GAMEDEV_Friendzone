245ec4baca41450c3a741e658d5bce28b99ca37c
zachysoba <zachraduban@gmail.com>
2016-03-26 23:24:51 +0800

    Added spin movement of black hole friend zone

---
+++ Assets/blackhole.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blackhole : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.back, speed * Time.deltaTime);
+	}
+}
