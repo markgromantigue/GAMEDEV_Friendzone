912ea166f9d693fe84b79ac26c58a13372951861
zachysoba <zachraduban@gmail.com>
2016-03-29 21:42:21 +0800

    Blackhole sprite and collider growth in time
    Added to script of blackhole.

--- Assets/blackhole.cs
+++ Assets/blackhole.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class blackhole : MonoBehaviour {
 
     public float speed = 10f;
+    public float scaleStep = 0;
 
+    private SphereCollider myCollider;
+
+    void Start() {
+        myCollider = transform.GetComponent<SphereCollider>();
+    }
 	// Update is called once per frame
 	void Update () {
         transform.Rotate(Vector3.back, speed * Time.deltaTime);
+
+        scaleStep += 0.001f;
+        transform.localScale = new Vector2(scaleStep, scaleStep);
+
+        myCollider.radius += 0.001f;
 	}
 }
