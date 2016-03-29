a6c05415c29d65c04380ad81680abac5e5c44903
markgromantigue <markg.romantigue@gmail.com>
2016-03-29 14:21:58 +0800

    Update spawner script

--- blah/Spawner.cs
+++ blah/Spawner.cs
@@ -29,3 +29,43 @@ public class Spawner : MonoBehaviour {
 	bool isDisabled;
 
 	public event System.Action<int> OnNewWave;
+    
+    void Start() {
+		playerEntity = FindObjectOfType<Player> ();
+		playerT = playerEntity.transform;
+
+		nextCampCheckTime = timeBetweenCampingChecks + Time.time;
+		campPositionOld = playerT.position;
+		playerEntity.OnDeath += OnPlayerDeath;
+
+		map = FindObjectOfType<MapGenerator> ();
+		NextWave ();
+	}
+
+	void Update() {
+		if (!isDisabled) {
+			if (Time.time > nextCampCheckTime) {
+				nextCampCheckTime = Time.time + timeBetweenCampingChecks;
+
+				isCamping = (Vector3.Distance (playerT.position, campPositionOld) < campThresholdDistance);
+				campPositionOld = playerT.position;
+			}
+
+			if ((enemiesRemainingToSpawn > 0 || currentWave.infinite) && Time.time > nextSpawnTime) {
+				enemiesRemainingToSpawn--;
+				nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+				StartCoroutine ("SpawnEnemy");
+			}
+		}
+
+		if (devMode) {
+			if (Input.GetKeyDown(KeyCode.Return)) {
+				StopCoroutine("SpawnEnemy");
+				foreach (Enemy enemy in FindObjectsOfType<Enemy>()) {
+					GameObject.Destroy(enemy.gameObject);
+				}
+				NextWave();
+			}
+		}
+	}
